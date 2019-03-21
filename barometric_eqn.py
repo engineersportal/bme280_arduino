ef77e8b87073e340b7df2e3638f7a7f058503c39
Engineer's Maker Portal <40637421+engineersportal@users.noreply.github.com>
2019-03-21 14:16:53 -0400

    Update barometric_eqn.py

--- barometric_eqn.py
+++ barometric_eqn.py
@@ -40,14 +40,16 @@ rho = (p_0/(R_d*T))+((h*p_sat)/(R_v*T))
 
 q = 1.4 # assumption: polytropic index is equal to ratio of specific heats
 
-T_lapse = 0.0065# temperature lapse rate for isothermal atmosphere
+T_lapse = -0.0065# temperature lapse rate for isothermal atmosphere
+
+lapse_q_diff = 9999.99
 
 p_range = np.arange(1500.0,500.0,-0.1)
-q_range = np.arange(0.4,2.0,0.2)
+q_range = np.arange(0.4,2.0,0.1)
 q_range = np.delete(q_range,np.argmin(np.abs(np.subtract(q_range,1.0))))
 fig = plt.figure(figsize=(14,8))
 for q in q_range:
-    z,z_taylor,z_isotherm,z_standard = [],[],[],[]
+    z,z_taylor,z_isotherm,z_lapse = [],[],[],[]
     for pres in p_range:
     ##p_meas = 1019.85
         p_meas = pres*100.0
@@ -60,7 +62,12 @@ for q in q_range:
         # isothermal assumption
         z_isotherm.append(-1.0*((p_0/(rho*g_phi))*np.log(p_meas/p_0)))
 
-        z_standard.append((((np.power(p_0/p_meas,(p_0*T_lapse)/(rho*T*g_phi)))*T)-T)/T_lapse)
+        z_lapse.append((T/T_lapse)*(1.0-((np.power(p_meas/p_0,(p_0*T_lapse)/(rho*T*g_phi))))))
+
+    if np.mean(np.abs(np.subtract(z_lapse,z_taylor)))<lapse_q_diff:
+        lapse_q_diff = np.mean(np.abs(np.subtract(z_lapse,z_taylor)))
+        lapse_q_opt = float(q)
+        
 ##    plt.plot(p_range,z,label='q = {0:2.2f} Exact'.format(q),linewidth=5,linestyle='--')
     if round(q,2)==1.4:
         plt.plot(p_range,z_taylor,label='q = {0:2.2f} (Adiabat)'.format(q),linewidth=3)
@@ -68,7 +75,7 @@ for q in q_range:
         plt.plot(p_range,z_taylor,label='q = {0:2.2f}'.format(q),linewidth=3)
 
 print('Mean Absolute Difference: {0:2.2f}'.format(np.mean(np.abs(np.subtract(z,z_taylor)))))
-plt.plot(p_range,z_standard,label='lapse rate',linewidth=3,linestyle='--')
+plt.plot(p_range,z_lapse,label='lapse rate',linewidth=3,linestyle='--')
 plt.plot(p_range,z_isotherm,label='q = 1.00 (Isotherm)',linestyle=':',linewidth=3)
 plt.ylim([0.0,1500.0])
 plt.xlim([850.0,1013.0])
@@ -80,13 +87,14 @@ ax1.axes.tick_params(axis='both',labelsize=16,pad=5)
 plt.tight_layout(pad=1.4)
 plt.savefig('altitude_adiabat_isotherm.png',dpi=200)
 
-plt.show()
+##plt.show()
 
 # analyzing the isothermal, adiabatic, and lapse rate profiles
 fig = plt.figure(figsize=(14,8))
 p_range = np.arange(1013.0,700.0,-0.1)
-z,z_taylor,z_isotherm,z_standard = [],[],[],[]
-q = 1.2
+z,z_taylor,z_isotherm,z_lapse = [],[],[],[]
+q = float(lapse_q_opt)
+print(q)
 for pres in p_range:
 ##p_meas = 1019.85
     p_meas = pres*100.0
@@ -99,15 +107,15 @@ for pres in p_range:
     # isothermal assumption
     z_isotherm.append(-1.0*((p_0/(rho*g_phi))*np.log(p_meas/p_0)))
 
-    z_standard.append((((np.power(p_0/p_meas,(p_0*T_lapse)/(rho*T*g_phi)))*T)-T)/T_lapse)
+    z_lapse.append((T/T_lapse)*(1.0-((np.power(p_meas/p_0,(p_0*T_lapse)/(rho*T*g_phi))))))
 ##    plt.plot(p_range,z,label='q = {0:2.2f} Exact'.format(q),linewidth=5,linestyle='--')
 
 plt.plot(p_range,z_taylor,label='q = {0:2.2f}'.format(q),linewidth=3)
 
-print('MAE Z-Taylor and Z-Lapse: {0:2.2f}'.format(np.mean(np.abs(np.subtract(z_taylor,z_standard)))))
-print('MAE Z-isotherm and Z-Lapse: {0:2.2f}'.format(np.mean(np.abs(np.subtract(z_standard,z_isotherm)))))
+print('MAE Z-Taylor and Z-Lapse: {0:2.2f}'.format(np.mean(np.abs(np.subtract(z_taylor,z_lapse)))))
+print('MAE Z-isotherm and Z-Lapse: {0:2.2f}'.format(np.mean(np.abs(np.subtract(z_lapse,z_isotherm)))))
 
-plt.plot(p_range,z_standard,label='Lapse Rate = -0.0065',linewidth=3,linestyle='--')
+plt.plot(p_range,z_lapse,label='Lapse Rate = -0.0065',linewidth=3,linestyle='--')
 plt.plot(p_range,z_isotherm,label='q = 1.00 (Isotherm)',linestyle=':',linewidth=3)
 ##plt.ylim([0.0,1500.0])
 ##plt.xlim([850.0,1013.0])
@@ -120,12 +128,12 @@ plt.tight_layout(pad=1.4)
 
 err_pts = 4
 for ii in range(0,err_pts):
-    err_range = p_range[ii*int(len(z_standard)/err_pts):(ii+1)*int(len(z_standard)/err_pts)]
-    taylor_range = z_taylor[ii*int(len(z_standard)/err_pts):(ii+1)*int(len(z_standard)/err_pts)]
-    lapse_range = z_standard[ii*int(len(z_standard)/err_pts):(ii+1)*int(len(z_standard)/err_pts)]
-    iso_range = z_isotherm[ii*int(len(z_standard)/err_pts):(ii+1)*int(len(z_standard)/err_pts)]
+    err_range = p_range[ii*int(len(z_lapse)/err_pts):(ii+1)*int(len(z_lapse)/err_pts)]
+    taylor_range = z_taylor[ii*int(len(z_lapse)/err_pts):(ii+1)*int(len(z_lapse)/err_pts)]
+    lapse_range = z_lapse[ii*int(len(z_lapse)/err_pts):(ii+1)*int(len(z_lapse)/err_pts)]
+    iso_range = z_isotherm[ii*int(len(z_lapse)/err_pts):(ii+1)*int(len(z_lapse)/err_pts)]
 
-    midpoint = int((((ii+1)*int(len(z_standard)/err_pts))-(ii*int(len(z_standard)/err_pts)))/2.0)
+    midpoint = int((((ii+1)*int(len(z_lapse)/err_pts))-(ii*int(len(z_lapse)/err_pts)))/2.0)
     # error at specific points
     mae_iso = np.sqrt(np.mean(np.power(np.subtract(iso_range,lapse_range),2.0)))
     mae_adiabat = np.sqrt(np.mean(np.power(np.subtract(taylor_range,lapse_range),2.0)))
