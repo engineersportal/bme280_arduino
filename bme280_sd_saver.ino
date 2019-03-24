2aafa6430df34fd24743872e7916b4b283a99809
Engineer's Maker Portal <40637421+engineersportal@users.noreply.github.com>
2019-03-24 16:33:16 -0400

    Update bme280_sd_saver.ino

--- bme280_sd_saver.ino
+++ bme280_sd_saver.ino
@@ -5,13 +5,17 @@
 #include <Adafruit_BME280.h>
 #include <math.h>
 
-File datafile; // initialize sd file
+File main_folder; // initialize folder for saving
+File dataFile; // initialize sd file
 const int chipSelect = 4; // CS pin on sd card module
+int prev_file_indx = 0; // used for file naming
+String fileName = "000";
 
 // measurements taken at r_0 (sea level)
-const float p_0 = 994.5*100.0;
-const float T = 7.0; // input temperature in C
-const float dew_pt = -1.0; // input dew pt
+const float station_elev = 11.0; // station elevation (KNYC used here)
+const float p_0 = 1017.3*100.0;
+const float T = 8.0; // input temperature in C
+const float dew_pt = -15.0; // input dew pt
 
 // constants related to gravity approx. and rho approx
 const float lat = 40.7128; // input latitude
@@ -38,6 +42,7 @@ const float L = -0.0065; // temperature lapse rate
 Adafruit_BME280 bme; // start BME sensor
 
 void setup() {
+//    Serial.begin(9600);
     // verify BME280 is working
     bool status;
     status = bme.begin();  
@@ -48,36 +53,64 @@ void setup() {
     if (!SD.begin(chipSelect)) {
       return;
     }
+    main_folder = SD.open("/");
+    fileName = sd_saver(main_folder);
 }
 
-void loop() {     
-    // polytropic calculation
-    float pres_ratio = bme.readPressure()/(p_0);
-    float z = B_coeff*(1.0-pow(pres_ratio,q_diff));
-    // temperature lapse calculation
-    float pow_val = (L*R_d)/g_phi;
-    float z_lapse = (Temp/L)*(1.0-(pow(pres_ratio,pow_val)));
-    
-    // SD save section
-    String data_array = "";
-    data_array += String(millis()); // save milliseconds since start of program
-    data_array += ",";
-    data_array += String(bme.readTemperature()); // save temp
-    data_array += ",";
-    data_array += String(bme.readHumidity()); // save humidity
-    data_array += ",";
-    data_array += String(bme.readPressure()); // save pressure in Pa
-    data_array += ",";
-    data_array += String(bme.readAltitude(p_0/100.0)); // save altitude from Adafruit routine
-    data_array += ",";
-    data_array += String(z); // save polytropic altitude calc.
+void loop() {
+  
+  // polytropic calculation
+  float pres_ratio = bme.readPressure()/(p_0);
+  float z = B_coeff*(1.0-pow(pres_ratio,q_diff));
+  // temperature lapse calculation
+  float pow_val = (L*R_d)/g_phi;
+  float z_lapse = (Temp/L)*(1.0-(pow(pres_ratio,pow_val)));
+  
+  // SD save section
+  String data_array = "";
+  data_array += String(millis()); // save milliseconds since start of program
+  data_array += ",";
+  data_array += String(bme.readTemperature()); // save temp
+  data_array += ",";
+  data_array += String(bme.readHumidity()); // save humidity
+  data_array += ",";
+  data_array += String(bme.readPressure()); // save pressure in Pa
+  data_array += ",";
+  data_array += String(bme.readAltitude(p_0/100.0)+station_elev); // save altitude from Adafruit routine
+  data_array += ",";
+  data_array += String(z+station_elev); // save polytropic altitude calc.
 
-    // SD Card writing and saving
-    datafile = SD.open("press5.csv", O_CREAT | O_WRITE);  
-    // if the file is valid, write to it:
-    if (datafile) {
-      datafile.println(data_array); // write data
-      datafile.close();
+  // SD Card writing and saving  
+  dataFile = SD.open("DATA"+fileName+".csv",FILE_WRITE);
+//  Serial.println("DATA"+fileName+".csv");
+//  Serial.println(z);
+  // if the file is valid, write to it:
+  if(dataFile){
+    dataFile.println(data_array);
+    dataFile.close();
+  }
+  delay(100);
+}
+
+String sd_saver(File dir){
+  while(true){
+    // iterate all files to ensure no overwrites
+    File entry = dir.openNextFile();
+    if (!entry){
+      break; // no more files, exist
     }
-    delay(100);
+    String entry_name = entry.name();
+    if ((entry_name.substring(4,7)).toInt()>=prev_file_indx){
+      prev_file_indx = (entry_name.substring(4,7)).toInt()+1;
+      if (prev_file_indx>=100){
+        fileName = String(prev_file_indx);
+      } else if (prev_file_indx>=10){
+        fileName = "0"+String(prev_file_indx);
+      } else{
+        fileName = "00"+String(prev_file_indx);
+      }
+    }
+    entry.close();
+  }
+  return fileName;
 }
